bee12dc32003818df2f55ce05b4b786561ad227d
GerogeGol <george.gol003@gmail.com>
2023-04-19 18:19:42 +0300

    triangle base implementation

--- src/core/Triangle/Triangle.h
+++ src/core/Triangle/Triangle.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "../Figure/Figure.h"
 
-class Triangle {
+class Triangle : private Figure {
+public:
+    Triangle() {
+        Triangle(Point{0, 1}, Point{1, 0}, {-1, 0});
+    }
+
+    Triangle(const Point point1, const Point point2, const Point point3) {
+        Segment seg1 = Segment(point1, point2);
+        Segment seg2 = Segment(point2, point3);
+        Segment seg3 = Segment(point3, point1);
+
+        Figure{seg1, seg2, seg3};
+    }
 };
-
