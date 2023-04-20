900fd468cadc7048a577fce22bc7099fc024985c
GerogeGol <george.gol003@gmail.com>
2023-04-20 18:53:00 +0300

    fix in triangle constructor

--- src/core/Triangle/Triangle.h
+++ src/core/Triangle/Triangle.h
@@ -11,5 +11,5 @@ public:
 
 
     Triangle(const Point &point1, const Point &point2, const Point &point3)
-        : Figure({Segment{point1, point2}, Segment{point2, point3}, Segment{point3, point3}}){};
+        : Figure({Segment{point1, point2}, Segment{point2, point3}, Segment{point3, point1}}){};
 };
