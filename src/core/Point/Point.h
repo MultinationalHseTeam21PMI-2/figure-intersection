b60c7c5c44c08a9f984a8c43cf5c12fceab7126a
GerogeGol <george.gol003@gmail.com>
2023-04-22 22:32:32 +0300

    moved Point class implementation

---
+++ src/core/Point/Point.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <memory>
+#include <stdexcept>
+
+
+#define PRECISION 0.001
+
+class Point {
+public:
+    double x, y;
+    explicit Point(double a = 0, double b = 0) {
+        x = a, y = b;
+    }
+    double norm() const {
+        return std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+    }
+    Point operator+(Point b) {
+        return Point(x + b.x, y + b.y);
+    }
+    Point operator-(Point b) {
+        return Point(x - b.x, x - b.y);
+    }
+    bool operator==(Point b) const {
+        return (std::abs(x - b.x) < PRECISION && std::abs(y - b.y) < PRECISION);
+    }
+};
+
+inline double norm(Point a) {
+    return std::sqrt(std::pow(a.x, 2) + std::pow(a.y, 2));
+}
