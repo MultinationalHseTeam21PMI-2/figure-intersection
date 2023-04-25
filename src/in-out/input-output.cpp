e59831c331fcbd91e7093d89dafbeabc949f4b54
i1uh4 <105651496+i1uh4@users.noreply.github.com>
2023-04-25 17:09:19 +0300

    Changed Entry Validation

--- src/in-out/input-output.cpp
+++ src/in-out/input-output.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <memory>
+#include "../core/Segment/Segment.h"
 
 typedef std::pair<double, double> Point;
 
 std::vector<std::vector<Point>> input() {
     int n;
-    std::cout << "Enter the number of shapes: ";
+    std::cout << "Enter the number of shapes:";
     std::cin >> n;
 
     std::vector<std::vector<Point>> shapes(n);
     for (int i = 0; i < n; i++) {
-        std::cout << "Enter the number of vertices for figure " << i + 1 << ":"
-                  << std::endl;
+        std::cout << "Enter the number of vertices for figure " << i + 1 << ":";
         int num_vertices;
         std::cin >> num_vertices;
 
@@ -22,38 +21,23 @@ std::vector<std::vector<Point>> input() {
 
         for (int j = 0; j < num_vertices; j++) {
             std::string input;
-
-            do {
                 std::cin >> input;
                 try {
                     vertices[j].first = input.empty() ? 0 : std::stod(input);
-                    if (vertices[j].first < 0) {
-                        std::cerr << "Warning: Negative value entered. Please enter a "
-                                     "non-negative value."
-                                  << std::endl;
-                    }
                 } catch (...) {
                     std::cerr << "Error: Incorrect value entered. Enter numeric value."
                               << std::endl;
                     return {};
                 }
-            } while (vertices[j].first < 0);
 
-            do {
                 std::cin >> input;
                 try {
                     vertices[j].second = input.empty() ? 0 : std::stod(input);
-                    if (vertices[j].second < 0) {
-                        std::cerr << "Warning: Negative value entered. Please enter a "
-                                     "non-negative value."
-                                  << std::endl;
-                    }
                 } catch (...) {
                     std::cerr << "Error: Incorrect value entered. Enter numeric value."
                               << std::endl;
                     return {};
                 }
-            } while (vertices[j].second < 0);
         }
         shapes[i] = vertices;
     }
@@ -62,10 +46,10 @@ std::vector<std::vector<Point>> input() {
 }
 
 
-void output(std::unique_ptr<Segment> segments) {
+void output(std::vector<Segment>& segments) {
     for (int i = 0; i < segments.size(); i++) {
-        Point point1 = segments[i][0];
-        Point point2 = segments[i][1];
+        Point point1 = segments[i].point1();
+        Point point2 = segments[i].point2();
         if (point1 == point2) {
             std::cout << "Triangle intersection point: (" << point1.first << ", " << point1.second << ")" << std::endl;
         }
