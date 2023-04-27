c2bf5ad0deac3a599b8d0e6b6a258afc5c85d02c
LaggyMan <45729776+LaggyMan@users.noreply.github.com>
2023-04-27 11:55:49 +0300

    Added two tests for intersection of segments

--- tests/test_core/test_intersection.cpp
+++ tests/test_core/test_intersection.cpp
@@ -15,14 +15,25 @@ TEST(Segment, test_segment_intersection_is_segment) {
 }
 
 TEST(Segment, test_segment_intersection_is_point) {
-    Point a(-6.67, 3.5), b(4.09, 4.62);
-    Point E(-5.03, 7.72), F(-2.39, 1.34);
-    Segment X(a, b);
-    Segment Z(E, F);
-    Segment Intersection_XY = *intersection(X, Z);
+    {
+        Point a(-6.67, 3.5), b(4.09, 4.62);
+        Point E(-5.03, 7.72), F(-2.39, 1.34);
+        Segment X(a, b);
+        Segment Z(E, F);
+        Segment Intersection_XY = *intersection(X, Z);
 
-    ASSERT_TRUE(Intersection_XY.isPoint());
-    ASSERT_TRUE(Intersection_XY.point1() == Point(-3.4236192700012, 3.8379132358363));
+        ASSERT_TRUE(Intersection_XY.isPoint());
+        ASSERT_TRUE(Intersection_XY.point1() == Point(-3.4236192700012, 3.8379132358363));
+    }
+    {
+        Point A(0,0), B(2,2), C(0,0), D(-2,2);
+        Segment first = Segment(A,B), second = Segment(C,D);
+        std::unique_ptr<Segment> Intersection_XY = intersection(first,second);
+
+        Segment expected_segment(A, A);
+        ASSERT_TRUE(Intersection_XY.isPoint())
+        ASSERT_EQ(Intersection_XY, expected_segment) 
+    }
 }
 
 TEST(Segment, test_segment_intersection) {
@@ -35,28 +46,20 @@ TEST(Segment, test_segment_intersection) {
 }
 
 TEST(Segment, test_segments_no_intersection) {
-    Point A(1, 1), B(2, 2), C(3, 3), D(4, 4);
-    Segment AB(A, B), CD(C, D);
+    {
+        Point A(1, 1), B(2, 2), C(3, 3), D(4, 4);
+        Segment AB(A, B), CD(C, D);
 
-    std::unique_ptr<Segment> intersection_AB_CD = intersection(AB, CD);
-    ASSERT_EQ(intersection_AB_CD.get(), nullptr);
-}
+        std::unique_ptr<Segment> intersection_AB_CD = intersection(AB, CD);
+        ASSERT_EQ(intersection_AB_CD.get(), nullptr);
+    }
 
-TEST(Segment, test_segments_no_intersection) {
-    Point A(1,1), B(2,2), C(-1,1), D(-2,2);
-    Segment AB = Segment(A,B), CD = Segment(C,D);
-    std::unique_ptr<Segment> Intersection_XY = intersection(first,second);
-    ASSERT_EQ(Intersection_XY, nullptr)
-}
-
-TEST(Segment, test_segment_intersection_is_point) {
-    Point A(0,0), B(2,2), C(0,0), D(-2,2);
-    Segment first = Segment(A,B), second = Segment(C,D);
-    std::unique_ptr<Segment> Intersection_XY = intersection(first,second);
-
-    Segment expected_segment(A, A);
-    ASSERT_TRUE(Intersection_XY.isPoint())
-    ASSERT_EQ(Intersection_XY, expected_segment)        
+    {
+        Point A(1,1), B(2,2), C(-1,1), D(-2,2);
+        Segment AB = Segment(A,B), CD = Segment(C,D);
+        std::unique_ptr<Segment> Intersection_XY = intersection(first,second);
+        ASSERT_EQ(Intersection_XY, nullptr)
+    }
 }
 
 // Intersection for figures
