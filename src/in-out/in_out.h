ef8049155aa7353b98040894d61a851cab6b4ec0
GerogeGol <george.gol003@gmail.com>
2023-04-28 18:24:56 +0300

    working prototype for 2 triangles

--- src/in-out/in_out.h
+++ src/in-out/in_out.h
@@ -1,15 +1,10 @@
-#ifndef FIGURE_INTERSECTION_IN_OUT_H
-#define FIGURE_INTERSECTION_IN_OUT_H
-
-
+#pragma once
+#include "../core/Segment/Segment.h"
 #include <iostream>
 #include <string>
 #include <vector>
-#include "../core/Segment/Segment.h"
 
 std::vector<std::vector<Point>> input();
 
 
-void output(std::vector<Segment>& segments);
-
-#endif //FIGURE_INTERSECTION_IN_OUT_H
+void output(std::vector<Segment> &segments);
