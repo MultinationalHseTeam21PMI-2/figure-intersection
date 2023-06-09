0f373d1da7a3dfbfd5012f503c7263475ea77352
i1uh4 <ily.expert@mail.ru>
2023-06-09 18:10:47 +0300

    Adding tests to verify that the vertices are inside another shape

--- src/core/Alghoritms/Alghoritms.h
+++ src/core/Alghoritms/Alghoritms.h
@@ -15,6 +15,10 @@
 /// @return nullptr if no intersection | Segment*
 std::unique_ptr<Segment> intersection(const Segment &a, const Segment &b);
 
+bool containsPoints(const std::vector<Point>& points, const Point& point);
+bool isVertexInsidePolygon(const Figure& polygon, const Point& point);
+std::vector<Point> findInternalVertices(const Figure& polygon1, const Figure& polygon2);
+
 /// @brief Method that finds intersections between two figures
 /// @param figure1
 /// @param figure2
---
+++ tests/test_core/test_in_point.cpp
@@ -0,0 +1,146 @@
+#include <gtest/gtest.h>
+#include "../../src/core/Alghoritms/Alghoritms.h"
+
+
+TEST(ContainsPointsTest, PointInPolygon) {
+   Figure polygon = { Segment(Point(3.0, 6.0), Point(-1, 2)),
+                      Segment(Point(-1, 2), Point(0, 10)),
+                      Segment(Point(0, 10), Point(3.0, 6.0))
+   };
+   Point point(1, 7.0);
+
+   bool result = isVertexInsidePolygon(polygon, point);
+
+   ASSERT_TRUE(result);
+}
+
+
+
+
+TEST(ContainsPointsTest, PointNotInPolygon) {
+   std::vector<Point> points = { Point(3.0, 4.0), Point(5.0, 6.0), Point(1.0, 2.0) };
+   Point point(7.0, 8.0);
+
+
+   bool result = containsPoints(points, point);
+
+
+   ASSERT_FALSE(result);
+}
+
+
+
+
+TEST(IsVertexInsidePolygonTest, VertexInsidePolygon) {
+   Figure polygon({ Segment(Point(0.0, 0.0), Point(1.0, 0.0)),
+                             Segment(Point(1.0, 0.0), Point(1.0, 1.0)),
+                             Segment(Point(1.0, 1.0), Point(0.0, 1.0)),
+                             Segment(Point(0.0, 1.0), Point(0.0, 0.0))
+   });
+   Point vertex(0.5, 0.5);
+
+
+   bool result = isVertexInsidePolygon(polygon, vertex);
+
+
+   ASSERT_TRUE(result);
+}
+
+
+
+
+TEST(FindInternalVerticesTest, InternalVertices) {
+   Figure polygon1({
+                     Segment(Point(2.0, 2.0), Point(4.0, 2.0)),
+                     Segment(Point(4.0, 2.0), Point(6, 4.0)),
+                     Segment(Point(6.0, 4.0), Point(4.0, 6.0)),
+                     Segment(Point(4.0, 6.0), Point(2.0, 6.0)),
+                     Segment(Point(2.0, 6.0), Point(0.0, 4.0)),
+                     Segment(Point(0.0, 4.0), Point(2.0, 2.0))
+   });
+
+
+   Figure polygon2({
+                     Segment(Point(2.0, 4.0), Point(4.0, 4.0)),
+                     Segment(Point(4.0, 4.0), Point(6, 6.0)),
+                     Segment(Point(6.0, 6.0), Point(4.0, 8.0)),
+                     Segment(Point(4.0, 8.0), Point(2.0, 8.0)),
+                     Segment(Point(2.0, 8.0), Point(0.0, 6.0)),
+                     Segment(Point(0.0, 6.0), Point(2.0, 4.0))
+   });
+
+
+   std::vector<Point> expected = { Point(2.0, 4.0), Point(4.0, 4.0),
+                                   Point(2.0, 6.0), Point(4.0, 6.0)
+   };
+   std::sort(expected.begin(), expected.end());
+
+
+   std::vector<Point> result = findInternalVertices(polygon1, polygon2);
+   std::sort(result.begin(), result.end());
+
+   ASSERT_EQ(expected, result);
+}
+
+
+
+
+TEST(FindInternalVerticesTest, NoInternalVertices) {
+   Figure polygon1({
+                     Segment(Point(2.0, 2.0), Point(4.0, 2.0)),
+                     Segment(Point(4.0, 2.0), Point(6, 4.0)),
+                     Segment(Point(6.0, 4.0), Point(4.0, 6.0)),
+                     Segment(Point(4.0, 6.0), Point(2.0, 6.0)),
+                     Segment(Point(2.0, 6.0), Point(0.0, 4.0)),
+                     Segment(Point(0.0, 4.0), Point(2.0, 2.0))
+   });
+
+
+   Figure polygon2({
+                     Segment(Point(8.0, 4.0), Point(10.0, 4.0)),
+                     Segment(Point(10.0, 4.0), Point(12, 6.0)),
+                     Segment(Point(12.0, 6.0), Point(10.0, 8.0)),
+                     Segment(Point(10.0, 8.0), Point(8.0, 8.0)),
+                     Segment(Point(8.0, 8.0), Point(6.0, 6.0)),
+                     Segment(Point(6.0, 6.0), Point(8.0, 4.0))
+   });
+
+   std::vector<Point> result = findInternalVertices(polygon1, polygon2);
+   std::sort(result.begin(), result.end());
+
+   ASSERT_EQ(0, result.size());
+}
+
+
+
+
+TEST(FindInternalVerticesTest, FigureInside) {
+   Figure polygon1({
+                     Segment(Point(2.0, 2.0), Point(4.0, 2.0)),
+                     Segment(Point(4.0, 2.0), Point(6, 4.0)),
+                     Segment(Point(6.0, 4.0), Point(4.0, 6.0)),
+                     Segment(Point(4.0, 6.0), Point(2.0, 6.0)),
+                     Segment(Point(2.0, 6.0), Point(0.0, 4.0)),
+                     Segment(Point(0.0, 4.0), Point(2.0, 2.0))
+   });
+
+
+   Figure polygon2({
+                     Segment(Point(2.0, 3.0), Point(4.0, 3.0)),
+                     Segment(Point(4.0, 3.0), Point(4, 5.0)),
+                     Segment(Point(4, 5.0), Point(2, 5)),
+                     Segment(Point(2, 5), Point(2.0, 3.0))
+   });
+
+
+   std::vector<Point> expected = { Point(2.0, 3.0), Point(4.0, 3.0),
+                                   Point(4.0, 5.0), Point(2.0, 5.0)
+   };
+   std::sort(expected.begin(), expected.end());
+
+
+   std::vector<Point> result = findInternalVertices(polygon1, polygon2);
+   std::sort(result.begin(), result.end());
+
+   ASSERT_EQ(expected, result);
+}
