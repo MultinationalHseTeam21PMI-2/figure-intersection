c9a3c9962ca122d5427d8fd407e8f8d5143e7ac6
i1uh4 <ily.expert@mail.ru>
2023-06-20 21:31:43 +0300

    Change src/main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,6 @@
-#include "core/Algorithms/Algorithms.h"
-#include "core/Segment/Segment.h"
-#include "core/Triangle/Triangle.h"
-#include "in-out/in_out.h"
+#include <iostream>
 
-int main() {
-
-    std::vector<std::vector<Point>> trianlges_points = input();
-    std::vector<Triangle> triangles;
-    for (auto triangle: trianlges_points) {
-        triangles.push_back(Triangle{triangle[0], triangle[1], triangle[2]});
-    }
-    std::vector<Segment> intersections;
-
-    std::vector<Segment> fig_intersections = intersection(triangles[0], triangles[1]);
-    intersections.insert(std::end(intersections), std::begin(fig_intersections), std::end(fig_intersections));
-
-    output(intersections);
-
+int main(){
 
     return 0;
 }
